9e69f806457ae434b2a1c132cccb12eb58e1eeb1
Anwana Ntofon <anwanantofon@gmail.com>
2024-11-02 20:32:10 -0700

    Updated tab title, installed npm package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import ConcertLanding from './components/ConcertLanding/ConcertLanding';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>My Concert</title>
+      </Helmet>
       <ConcertLanding />
     </div>
   );
